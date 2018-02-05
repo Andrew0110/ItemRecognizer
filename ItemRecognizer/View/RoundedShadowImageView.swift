43fb7cb211adab0f9f28d975b32b17c25bdd043e
Andrew <r.andrew0110@gmail.com>
2018-02-05 12:35:02 +0200

    first commit

---
+++ ItemRecognizer/View/RoundedShadowImageView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedShadowImageView.swift
+//  ItemRecognizer
+//
+//  Created by Andrew on 11.01.18.
+//  Copyright © 2018 AR. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
