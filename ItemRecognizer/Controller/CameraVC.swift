d5358d5a088455476aae6286358dac6395beb98d
Andrew <r.andrew0110@gmail.com>
2018-02-05 12:38:12 +0200

    Initial

--- ItemRecognizer/Controller/CameraVC.swift
+++ ItemRecognizer/Controller/CameraVC.swift
@@ -7,19 +7,194 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+enum FlashState {
+    case off
+    case on
 }
 
+class CameraVC: UIViewController {
+ 
+    // MARK: - Properties
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    var photoData: Data?
+    
+    var flashControlState: FlashState = .off
+    
+    var speechSynthesizer = AVSpeechSynthesizer()
+
+    @IBOutlet weak var captureImageView: RoundedShadowImageView!
+    @IBOutlet weak var flashBtn: RoundedShadowButton!
+    @IBOutlet weak var identificationLbl: UILabel!
+    @IBOutlet weak var confidenceLbl: UILabel!
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var labelsView: RoundedShadowView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+   
+    // MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCamera))
+        tap.numberOfTapsRequired = 1
+        
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+            cameraOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.addOutput(cameraOutput!)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+                previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                
+                cameraView.layer.addSublayer(previewLayer!)
+                
+                cameraView.addGestureRecognizer(tap)
+                
+                captureSession.startRunning()
+            }
+        } catch {
+            debugPrint(error)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+        speechSynthesizer.delegate = self
+        activityIndicator.isHidden = true
+    }
+    
+    // MARK: - preparations
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - Actions
+    @objc func didTapCamera(){
+        self.cameraView.isUserInteractionEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        let settings = AVCapturePhotoSettings()
+//        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+//        let previewFormat = [
+//            kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+//            kCVPixelBufferWidthKey as String: 227,
+//            kCVPixelBufferHeightKey as String: 227]
+        
+//        settings.previewPhotoFormat = previewFormat
+        
+        settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
+        
+        if flashControlState == .off {
+            settings.flashMode = .off
+        } else {
+            settings.flashMode = .on
+        }
+        
+        cameraOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func resultsMethod(request:VNRequest, error:Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else {
+            return
+        }
+        
+        // choose maximum confidence in future
+        for classification in results {
+            if classification.confidence < 0.5 {
+//                let unknownObjectMessage = "I'm not sure what is this. Please try again"
+                let unknownObjectMessage = "Не могу разобрать. Попробуйте еще раз."
+                self.identificationLbl.text = unknownObjectMessage
+                synthesizeSpeech(fromString: unknownObjectMessage)
+                self.confidenceLbl.text = ""
+                break
+            } else {
+                let identifier = classification.identifier
+                let confidence = Int(classification.confidence*100)
+                self.identificationLbl.text = identifier
+                self.confidenceLbl.text = "CONFIDENCE: \(confidence)%"
+                let completeSentence = "This looks like a \(identifier) and I'm \(confidence) percent sure."
+                synthesizeSpeech(fromString: completeSentence)
+                break
+            }
+        }
+    }
+    
+    func synthesizeSpeech(fromString string:String) {
+        let speechUtterance = AVSpeechUtterance(string: string)
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+
+        speechSynthesizer.speak(speechUtterance)
+    }
+    
+    @IBAction func flashBtnWasPressed(_ sender: Any) {
+        switch flashControlState {
+        case .off:
+            flashBtn.setTitle("FLASH ON", for: .normal)
+            flashControlState = .on
+        case .on:
+            flashBtn.setTitle("FLASH OFF", for: .normal)
+            flashControlState = .off
+        }
+    }
+    
+}
+
+// MARK: - AVCapturePhotoCaptureDelegate
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        } else {
+            photoData = photo.fileDataRepresentation()
+            
+            do {
+                let model = try VNCoreMLModel(for: SqueezeNet().model)
+
+                let request = VNCoreMLRequest(model: model, completionHandler: resultsMethod)
+                let handler = VNImageRequestHandler(data: photoData!)
+                
+                try handler.perform([request])
+            } catch {
+                debugPrint(error)
+            }
+            
+            let image = UIImage(data: photoData!)
+            self.captureImageView.image = image
+        }
+    }
+}
+
+// MARK: - AVSpeechSynthesizerDelegate
+extension CameraVC: AVSpeechSynthesizerDelegate {
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        activityIndicator.isHidden = true
+        activityIndicator.stopAnimating()
+        self.cameraView.isUserInteractionEnabled = true
+    }
+}
